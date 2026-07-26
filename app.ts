985dc92bdc282c117f1460f84a88da4377d42441
Gilson96 <grafael99@gmail.com>
2026-07-26 01:54:47 +0100

    Scaffolding project/server and MongoDB connection

---
+++ app.ts
@@ -0,0 +1,30 @@
+import express, { type Application } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./dbConnection.ts";
+import mongoose from "mongoose";
+import routes from "./routes/routes.ts";
+
+dotenv.config();
+
+connectDB();
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", routes);
+
+const PORT = process.env.PORT || 5050;
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
