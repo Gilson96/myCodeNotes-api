384bb61e908de6324358fe26831feb16fad62de4
Gilson96 <grafael99@gmail.com>
2026-07-26 16:34:42 +0100

    fix moongose connection

--- app.ts
+++ app.ts
@@ -16,13 +16,8 @@ app.use(express.json());
 
 app.use("/", routes);
 
-const PORT = process.env.PORT || 5050;
-
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
 });
 
 mongoose.connection.on("error", (err) => {
